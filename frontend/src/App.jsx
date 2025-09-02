3679ffb4f5d9c61225e926220b262ce67b742f5d
saniya <saniyagarg9@gmail.com>
2025-09-03 04:03:09 +0530

    added the ui using react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React,{useRef,useContext} from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+const App = () => {
+  const {audioRef}=useContext(PlayerContext)
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar/>
+        <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} preload="auto"></audio>
+      
+    </div>
+  )
+}
+
+export default App
